98580350c61ccf18b022b81ae8015edde998172d
georgepetru <georgepetru@DESKTOP-LIHJRQP>
2026-01-21 09:56:17 +0200

    50 - Install Carter, Develop Create Product with Minimal Apis and Carter

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,18 +1,35 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
 
 namespace Catalog.API.Products.CreateProduct;
 
-public record CreateProductCommand(string Name,  List<string> Categry, string Description, string ImageFile, decimal Price)
-    : IRequest<CreateProductResult>;
+public record CreateProductCommand(string Name,  List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
 
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
         //Business logic to create a product
 
-        throw new NotImplementedException();
+        //Create Product entity from commnad object
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price,
+        };
+
+        //save to database
+
+        //return result
+        return new CreateProductResult(Guid.NewGuid());
+
+        //throw new NotImplementedException();
     }
 }
